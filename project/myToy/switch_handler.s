bc3eaaeee0a9621ed52b331643e565239b49275f
frux-c <frux.infoc@gmail.com>
2023-05-06 12:59:02 -0600

    simplified asm

--- project/myToy/switch_handler.s
+++ project/myToy/switch_handler.s
@@ -4,45 +4,35 @@
 
 	.global switch_interrupt_handler
 	.extern P2IN,P2IES,P2IFG,SWITCHES,SW1,SW2,SW3,SW4,sw_high
-
 	switch_interrupt_handler:
-	    ; Load P2IFG and mask with SWITCHES
-	    mov.b &P2IFG, r15
-	    and.b &SWITCHES, r15
-
-	    ; Load P2IN
-	    mov.b P2IN, r14
-
-	    ; Update interrupt sense based on button state
-	    bit.b &SW1, r14
-			bit.b &SW2, r14
-			bit.b &SW3, r14
-			bit.b &SW4, r14
-	    jnz switch_interrupt_handler_skip_update
-	    bic.b &SWITCHES, &P2IES
-	    bis.b r15, &P2IES
-	switch_interrupt_handler_skip_update:
-
-	    ; Check which button was pressed and update sw_high
-	    bit.b &SW1, r15
-	    jnz switch_interrupt_handler_sw1
-	    bit.b &SW2, r15
-	    jnz switch_interrupt_handler_sw2
-	    bit.b &SW3, r15
-	    jnz switch_interrupt_handler_sw3
-	    bit.b &SW4, r15
-	    jnz switch_interrupt_handler_sw4
-	    mov.b #0, &sw_high ; No button pressed
-	    reti
+			;update switch interrupt sense to detect changes from current buttons
+			;if switch is up, sense down
+		;	bis.b &P2IES, &P2IN
+		;	bis.b &P2IES, #0xf
+			;if switch down, sense up
+		;	bit.b &P2IES, &P2IN
+		;	bic.b &P2IES, #0xf
+			
+			mov #1, &sw_high
+			cmp.b &SW1, &P2IN
+			jz switch_interrupt_handler_sw1
+			cmp.b &SW2, &P2IN
+			jz switch_interrupt_handler_sw2
+			cmp.b &SW3, &P2IN
+			jz switch_interrupt_handler_sw3
+			cmp.b &SW4, &P2IN
+			jz switch_interrupt_handler_sw4
+			reti
+			
 	switch_interrupt_handler_sw1:
-	    mov.b #1, &sw_high
+	    mov #1, &sw_high
 	    reti
 	switch_interrupt_handler_sw2:
-	    mov.b #2, &sw_high
+	    mov #2, &sw_high
 	    reti
 	switch_interrupt_handler_sw3:
-	    mov.b #3, &sw_high
+	    mov #3, &sw_high
 	    reti
 	switch_interrupt_handler_sw4:
-	    mov.b #4, &sw_high
+	    mov #4, &sw_high
 	    reti
